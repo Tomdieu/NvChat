702894375829fd2bf281559f270ac89c01437f2c
Ivan Tom <77198289+Tomdieu@users.noreply.github.com>
2023-02-09 20:20:59 +0100

    updated

--- frontend/android/App.js
+++ frontend/android/App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.text}>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +17,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text:{
+    fontSize:20,
+    fontWeight:"600"
+  }
 });
