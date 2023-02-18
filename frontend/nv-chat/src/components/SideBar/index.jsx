c065f2d81be462a6157dc03b8151ee0e84fed9c1
Ivan Tom <77198289+Tomdieu@users.noreply.github.com>
2023-02-18 23:05:13 +0100

    updated

--- frontend/nv-chat/src/components/SideBar/index.jsx
+++ frontend/nv-chat/src/components/SideBar/index.jsx
@@ -1,12 +1,22 @@
 import { COLORS } from "@constants/index";
-import { MenuOutlined } from "@mui/icons-material";
-import { Box, IconButton, Typography } from "@mui/material";
+import { ChatOutlined, Group, GroupWork, MenuOutlined, Search } from "@mui/icons-material";
+import { Box, IconButton, TextField, Typography } from "@mui/material";
 import React, { useState } from "react";
-import { Sidebar, Menu, MenuItem,useProSidebar  } from "react-pro-sidebar";
+import { Sidebar, Menu, MenuItem, useProSidebar } from "react-pro-sidebar";
+
+import { makeStyles } from "@mui/styles";
+
+const useStyle = makeStyles((theme) => ({
+  sidebar: {
+    backgroundColor: "red",
+    height: "100vh",
+  },
+}));
 
 const index = () => {
   const [isCollapsed, setIsCollapsed] = useState(true);
   const { collapseSidebar, rtl } = useProSidebar();
+  const classes = useStyle();
   return (
     <Box
       sx={{
@@ -21,30 +31,41 @@ const index = () => {
         "& .pro-icon-wrapper": {
           backgroundColor: "transparent !important",
         },
-        "& .pro-inner-item": {
-          padding: "5px 35px 5px 20px !important",
-        },
-        "& .pro-innner-item:hover": {
-          color: "#8668fd !important",
-        },
-        "& .pro-menu-item.active": {
-          color: "#6870fa !important",
-        },
+        // "& .pro-inner-item": {
+        //   padding: "5px 35px 5px 20px !important",
+        // },
+        // "& .pro-innner-item:hover": {
+        //   color: "#8668fd !important",
+        // },
+        // "& .pro-menu-item.active": {
+        //   color: "#6870fa !important",
+        // },
       }}
     >
-      
-      <Sidebar style={{ height: "100vh",backgroundColor:'navy' }}>
+      <Sidebar className={classes.sidebar}>
         <Menu>
           <MenuItem
-            icon={<MenuOutlined style={{cursor:'pointer'}}/>}
+            icon={<MenuOutlined style={{ cursor: "pointer", color: "#fff" }} />}
             onClick={() => {
               collapseSidebar();
             }}
-        
-            style={{ textAlign: "center",cursor:'pointer' }}
+            style={{ textAlign: "center", cursor: "pointer" }}
           >
             {" "}
-            <h2>Admin</h2>
+            <Typography sx={{ color: "#fff" }} variant={"h4"}>
+              Nv Chat
+            </Typography>
+          </MenuItem>
+          <MenuItem
+            icon={<ChatOutlined style={{ cursor: "pointer", color: "#fff" }} />}
+          >
+    <Typography>
+    Chat
+
+    </Typography>
+          </MenuItem>
+          <MenuItem icon={<Group style={{ cursor: "pointer", color: "#fff" }}/>}>
+            <Typography>Group</Typography>
           </MenuItem>
         </Menu>
       </Sidebar>
