216d7763309af27e9273b3f1dafdf723cf54a6af
Ivan Tom <77198289+Tomdieu@users.noreply.github.com>
2023-02-19 22:10:02 +0100

    created account app

---
+++ backend/account/api/serializers.py
@@ -0,0 +1,37 @@
+from rest_framework import serializers
+
+from django.contrib.auth import get_user_model
+
+from django.db.models import Q
+
+from account.models import UserProfile
+
+User = get_user_model()
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = [
+            "username",
+            "email",
+            "password",
+        ]
+
+        extra_kwargs = {"password": {"write_only": True}}
+
+
+class UserProfileSerializer(serializers.ModelSerializer):
+    user = UserSerializer()
+
+    class Meta:
+        model = UserProfile
+        fields = "__all__"
+
+    def create(self, validated_data):
+        user_data = validated_data.pop("user")
+        user = User.objects.create(**user_data)
+        validated_data["user"] = user
+        profile = UserProfile.objects.create(**validated_data)
+
+        return profile
---
+++ backend/account/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+# Create your models here.
+
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE,related_name="profile")
+    phone_number = models.CharField(max_length=255,blank=True,null=True)
+    country = models.CharField(max_length=255,blank=True,null=True)
+    profile_picture = models.ImageField(upload_to="profiles/", null=True, blank=True)
+    bio = models.CharField(max_length=255,null=True,blank=True)
+
+    def __str__(self) -> str:
+        return f"{self.user}"
