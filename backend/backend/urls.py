a7cddee8ef7695f2677f106bfbd6e2be8b527905
Ivan Tom <77198289+Tomdieu@users.noreply.github.com>
2023-02-21 22:47:32 +0100

    updated backend urls

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -38,7 +38,39 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path(
+        "api/",
+        include(
+            [
+                path("chat/", include("chat.urls")),
+                path("account/", include("account.urls")),
+                re_path(
+                    r"^docs/",
+                    include(
+                        [
+                            re_path(
+                                r"^swagger(?P<format>\.json|\.yaml)$",
+                                schema_view.without_ui(cache_timeout=0),
+                                name="schema-json",
+                            ),
+                            re_path(
+                                r"^swagger/$",
+                                schema_view.with_ui("swagger", cache_timeout=0),
+                                name="schema-swagger-ui",
+                            ),
+                            re_path(
+                                r"^redoc/$",
+                                schema_view.with_ui("redoc", cache_timeout=0),
+                                name="schema-redoc",
+                            ),
+                        ]
+                    ),
+                ),
+            ]
+        ),
+    ),
     path("chat/", include("chat.urls")),
+    path("account/", include("account.urls")),
     # API DOCS ROUTES
     re_path(
         r"^swagger(?P<format>\.json|\.yaml)$",
