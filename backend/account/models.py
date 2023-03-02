1f86186fc2c230e5789ac87aaf6ccad8f8a4efd2
Ivan Tom <77198289+Tomdieu@users.noreply.github.com>
2023-03-02 22:32:49 +0100

    updated accounts models added online column to userprofile

--- backend/account/models.py
+++ backend/account/models.py
@@ -6,12 +6,14 @@ from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
+
 class UserProfile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE,related_name="profile")
-    phone_number = models.CharField(max_length=255,blank=True,null=True)
-    country = models.CharField(max_length=255,blank=True,null=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="profile")
+    phone_number = models.CharField(max_length=255, blank=True, null=True)
+    country = models.CharField(max_length=255, blank=True, null=True)
     profile_picture = models.ImageField(upload_to="profiles/", null=True, blank=True)
-    bio = models.CharField(max_length=255,null=True,blank=True)
+    bio = models.CharField(max_length=255, null=True, blank=True)
+    online = models.BooleanField(default=False)
 
     def __str__(self) -> str:
         return f"{self.user}"
