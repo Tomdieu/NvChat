e63aa7d9cab53fd5f26a653fe872b458444019cc
Ivan Tom <77198289+Tomdieu@users.noreply.github.com>
2023-03-05 20:56:12 +0100

    updated signals file

--- backend/account/signals.py
+++ backend/account/signals.py
@@ -6,10 +6,20 @@ from django.contrib.auth import get_user_model
 
 from rest_framework.authtoken.models import Token
 
+from .models import UserProfile
+
 User = get_user_model()
 
-@receiver(post_save,sender=User)
-def create_user_token(sender,instance,created,**kwargs):
+
+@receiver(post_save, sender=User)
+def create_user_token(sender, instance, created, **kwargs):
     if created:
         Token.objects.create(user=instance)
-        
+
+
+@receiver(post_save, sender=UserProfile)
+def create_friend_list(sender, instance, created, **kwargs):
+    if created:
+        from friends.models import FriendList
+
+        FriendList.objects.create(user=instance)
