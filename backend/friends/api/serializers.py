59d8e96b47e076107f8c7f0a321bea5ac6d1621e
Ivan Tom <77198289+Tomdieu@users.noreply.github.com>
2023-03-05 20:42:43 +0100

    created SearchFriendSerializer

--- backend/friends/api/serializers.py
+++ backend/friends/api/serializers.py
@@ -1,6 +1,15 @@
 from rest_framework import serializers
+
 from ..models import FriendList, FriendRequest
-from account.api.serializers import UserProfileSerializer
+from account.models import UserProfile
+from django.contrib.auth import get_user_model
+
+from django.db.models import Q
+
+from account.api.serializers import UserProfileSerializer, UserSerializer
+from .utils import get_friend_request_or_false
+
+User = get_user_model()
 
 
 class FriendListSerializer(serializers.ModelSerializer):
@@ -16,6 +25,7 @@ class FriendRequestCreateSerializer(serializers.ModelSerializer):
     class Meta:
         model = FriendRequest
         fields = "__all__"
+        extra_kwargs = {"sender": {"read_only": True}}
 
 
 class FriendRequestSerializer(serializers.ModelSerializer):
@@ -25,3 +35,78 @@ class FriendRequestSerializer(serializers.ModelSerializer):
     class Meta:
         model = FriendRequest
         fields = "__all__"
+
+
+class SearchFriendSerializer(serializers.ModelSerializer):
+    is_friend = serializers.SerializerMethodField()
+    is_self = serializers.SerializerMethodField()
+    you_send_friend_request = serializers.SerializerMethodField()
+    them_send_friend_request = serializers.SerializerMethodField()
+    no_request_send = serializers.SerializerMethodField()
+    friend_request = serializers.SerializerMethodField()
+
+    user = UserSerializer(read_only=True)
+
+    friend = serializers.SerializerMethodField()
+
+    class Meta:
+        model = UserProfile
+        fields = "__all__"
+
+    def get_is_friend(self, obj: UserProfile):
+        friend_list: FriendList = self.context["request"].user.profile.friend_list
+        return obj in friend_list.friends.all()
+
+    def get_is_self(self, obj: UserSerializer):
+        return obj == self.context["request"].user.profile
+
+    def get_you_send_friend_request(self, obj: UserProfile):
+        friend_request = FriendRequest.objects.filter(
+            sender=self.context["request"].user.profile, reciever=obj, is_active=True
+        )
+        if friend_request.exists():
+            return True
+        return False
+
+    def get_them_send_friend_request(self, obj: UserProfile):
+        friend_request = FriendRequest.objects.filter(
+            sender=obj, reciever=self.context["request"].user.profile, is_active=True
+        )
+        if friend_request.exists():
+            return True
+        return False
+
+    def get_no_request_send(self, obj: UserProfile):
+        friend_request = FriendRequest.objects.filter(
+            (Q(sender=obj) & Q(reciever=self.context["request"].user.profile))
+            | (Q(sender=self.context["request"].user.profile) & Q(reciever=obj))
+        )
+
+        print(friend_request)
+
+        if friend_request.exists():
+            return False
+        return True
+
+    def get_friend(self, obj: UserProfile):
+        friends: UserProfile = obj.friend_list.friends.all()
+        # print(friends)
+        if friends:
+            serializer = UserProfileSerializer(friends, context=self.context, many=True)
+            return serializer.data
+        return []
+
+    def pending_friend_request_id(self, obj: UserProfile):
+        result = get_friend_request_or_false(
+            sender=obj, reciever=self.context["request"].user.profile
+        )
+        if result != False:
+            return result.id
+
+    def get_friend_request(self, obj: UserProfile):
+        if obj == self.context["request"].user.profile:
+            friend_request = FriendRequest.objects.filter(
+                sender=self.context["request"].user.profile, is_active=True
+            )
+            return FriendRequestSerializer(friend_request, many=True).data
+        return None
