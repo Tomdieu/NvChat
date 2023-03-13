93e54a626b5e49bc58569547e36d506823558262
Ivan Tom <77198289+Tomdieu@users.noreply.github.com>
2023-03-13 22:40:35 +0100

    updated urlss.py file

--- backend/chat/api/urls.py
+++ backend/chat/api/urls.py
@@ -4,11 +4,21 @@ from . import views
 
 router = DefaultRouter()
 
-router.register("groups",views.ChatGroupViewSet,basename="groups")
-router.register("join-group",views.JoinGroupViewSet,basename="join-group")
-router.register("chat-group-message",views.GroupChatMessageViewSet,basename="chat-group-message")
-router.register("conversation",views.ConversationViewSet,basename="conversations")
-router.register("messages",views.MessageViewSet,basename="messages")
+router.register("groups", views.ChatGroupViewSet, basename="groups")
+router.register(
+    "add-group-member", views.AddMemberToGroupViewSet, basename="add-group-member"
+)
+router.register(
+    "remove-group-member",
+    views.removeMemberFromGroupViewSet,
+    basename="remove-group-member",
+)
+router.register("join-group", views.JoinGroupViewSet, basename="join-group")
+router.register(
+    "chat-group-message", views.GroupChatMessageViewSet, basename="chat-group-message"
+)
+router.register("conversation", views.ConversationViewSet, basename="conversations")
+router.register("messages", views.MessageViewSet, basename="messages")
 
 urlpatterns = []
 
