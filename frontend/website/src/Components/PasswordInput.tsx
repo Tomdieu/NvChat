772140d2252e6413bc5514aad99c4d6ad48be0a9
Ivan Tom <77198289+Tomdieu@users.noreply.github.com>
2023-03-15 08:03:39 +0100

    created password input component

---
+++ frontend/website/src/Components/PasswordInput.tsx
@@ -0,0 +1,44 @@
+import { RemoveRedEye } from "@mui/icons-material";
+import { Box, IconButton, TextField, TextFieldProps } from "@mui/material";
+import { useState } from "react";
+
+type Props = {} & TextFieldProps;
+
+const PasswordInput = (props: Props) => {
+  const { value, ...other } = props;
+  const [type, setType] = useState<"text" | "password">("password");
+  const toggleType = () => {
+    if (type === "password") return setType("text");
+    setType("password");
+  };
+  return (
+    <Box sx={{ width: "100%", position: "relative" }}>
+      <TextField type={type} fullWidth {...other} />
+      <Box
+        sx={{
+          position: "absolute",
+          top: 2,
+          right: 1.8,
+          bottom: 2,
+          zIndex: 999,
+          borderRadius: 2,
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          backgroundColor: "rgba(255,255,255,.3)",
+          backdropFilter: "blur(8px)",
+          p: 0.2,
+          // lineHeight: 32,
+        }}
+      >
+        {value && (
+          <IconButton onClick={toggleType}>
+            <RemoveRedEye />
+          </IconButton>
+        )}
+      </Box>
+    </Box>
+  );
+};
+
+export default PasswordInput;
