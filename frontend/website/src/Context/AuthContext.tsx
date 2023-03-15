599f29f621f9b4eb08a7e2110eeed2e87bcf6271
Ivan Tom <77198289+Tomdieu@users.noreply.github.com>
2023-03-15 23:04:06 +0100

    updated AuthContext

--- frontend/website/src/Context/AuthContext.tsx
+++ frontend/website/src/Context/AuthContext.tsx
@@ -1,3 +1,5 @@
+import { Alert, AlertColor, SnackbarCloseReason } from "@mui/material";
+import SnackBar from "Components/SnackBar";
 import ApiService from "Utils/ApiService";
 import React, { createContext, useContext, useEffect, useState } from "react";
 import { UserProfile } from "types/UserProfile";
@@ -9,9 +11,19 @@ type AuthContextType = {
   setUserProfile: React.Dispatch<React.SetStateAction<UserProfile>>;
   login: (username: string, password: string) => Promise<Response>;
   logout: () => void;
+  setSeverity: React.Dispatch<React.SetStateAction<AlertColor>>;
+  setIcon: React.Dispatch<React.SetStateAction<React.ReactNode>>;
+  setOpen: React.Dispatch<React.SetStateAction<boolean>>;
+  setMessage: React.Dispatch<React.SetStateAction<string>>;
+  showBar: (
+    message: string,
+    icon: React.ReactNode,
+    severity: AlertColor
+  ) => void;
 };
 
 export const AuthContext = createContext<AuthContextType>(null);
+AuthContext.displayName = "AuthContext";
 
 export const useAuth = () => useContext(AuthContext);
 
@@ -24,10 +36,16 @@ export const AuthContextProvider = (props: Props) => {
   const [userToken, setUserToken] = useState<string | null>(null);
   const [userProfile, setUserProfile] = useState<UserProfile | null>(null);
 
+  const [severity, setSeverity] = useState<AlertColor>("success");
+  const [icon, setIcon] = useState<React.ReactNode>(null);
+  const [open, setOpen] = React.useState(false);
+  const [message, setMessage] = useState(null);
+
   const login = async (username: string, password: string) => {
     const res = await ApiService.login(username, password);
     return res;
   };
+
   const logout = () => {
     setUserProfile(null);
     setUserToken(null);
@@ -58,6 +76,30 @@ export const AuthContextProvider = (props: Props) => {
     }
   }, []);
 
+  const handleClose = (
+    event: Event | React.SyntheticEvent<any, Event>,
+    reason: SnackbarCloseReason
+  ) => {
+    if (reason === "clickaway") {
+      return;
+    }
+
+    setOpen(false);
+  };
+  const handleCloseAlert = (event: React.SyntheticEvent<Element, Event>) => {
+    setOpen(false);
+  };
+
+  const showBar = (
+    message: string,
+    icon: React.ReactNode,
+    severity: AlertColor
+  ) => {
+    setOpen(true);
+    setMessage(message);
+    setIcon(icon);
+    setSeverity(severity);
+  };
   return (
     <AuthContext.Provider
       value={{
@@ -67,9 +109,31 @@ export const AuthContextProvider = (props: Props) => {
         setUserProfile,
         login,
         logout,
+        setSeverity,
+        setIcon,
+        setOpen,
+        setMessage,
+        showBar,
       }}
     >
-      {children}
+      <>
+        {children}
+        <SnackBar open={open} autoHideDuration={6000} onClose={handleClose}>
+          <Alert
+            severity={severity}
+            icon={icon}
+            onClose={handleCloseAlert}
+            sx={(theme) => ({
+              padding: theme.spacing(2),
+              fontSize: 18,
+              alignItems: "center",
+              justifyContent: "space-between",
+            })}
+          >
+            {message}
+          </Alert>
+        </SnackBar>
+      </>
     </AuthContext.Provider>
   );
 };
