660b9252a4325e977cc4ef48829af310cbb666aa
Ivan Tom <77198289+Tomdieu@users.noreply.github.com>
2023-03-16 21:25:45 +0100

    updated added useEffect to check loggin user

--- frontend/website/src/pages/chat/group.tsx
+++ frontend/website/src/pages/chat/group.tsx
@@ -1,14 +1,26 @@
 import { Grid } from "@mui/material";
 import GroupSidebar from "Components/Chat/GroupSidebar/GroupSidebar";
 import GroupChat from "Components/Chat/GroupChat/GroupChat";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import GroupRightbar from "Components/Chat/GroupRightbar/GroupRightbar";
 
 type Props = {};
 
 const group = (props: Props) => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const token = localStorage.getItem("userToken");
+    if (!token) {
+      navigate("/auth/login");
+    }
+  }, []);
   return (
     <Grid container>
       <GroupSidebar />
       <GroupChat />
+      <GroupRightbar />
     </Grid>
   );
 };
