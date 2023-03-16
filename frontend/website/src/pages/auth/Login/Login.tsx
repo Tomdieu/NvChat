5bcfdcc93943f6814cfa42d68765786937080857
Ivan Tom <77198289+Tomdieu@users.noreply.github.com>
2023-03-16 21:26:12 +0100

    added useEffect to check user loggedin

--- frontend/website/src/pages/auth/Login/Login.tsx
+++ frontend/website/src/pages/auth/Login/Login.tsx
@@ -1,13 +1,22 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useStyles } from "./styles";
 import { Grid } from "@mui/material";
 
 import LoginComponent from "Components/auth/Login";
+import { useNavigate } from "react-router-dom";
 
 type Props = {};
 
 const Login = (props: Props) => {
   const classes = useStyles();
+  const navigate = useNavigate();
+  useEffect(() => {
+    const token = localStorage.getItem("userToken");
+    if (token) {
+      return navigate("/");
+    }
+  }, []);
+
   return <LoginComponent />;
 };
 
