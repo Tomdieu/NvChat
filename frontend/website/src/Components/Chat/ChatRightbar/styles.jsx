d94ecbef294e7853d957440733137ddd7c5fadfb
Ivan Tom <77198289+Tomdieu@users.noreply.github.com>
2023-03-29 22:02:09 +0200

    updated extension from tsx to jsx

---
+++ frontend/website/src/Components/Chat/ChatRightbar/styles.jsx
@@ -0,0 +1,46 @@
+import { makeStyles } from "@mui/styles";
+
+export const useStyles = makeStyles((theme) => ({
+  rightbar: {
+    height: "100vh",
+    backgroundColor: "#e1dbdb",
+    width: "100%",
+  },
+  rightbarWrapper: {
+    height: "100%",
+    display: "flex",
+    flexDirection: "column",
+    width: "100%",
+  },
+  rightbarTop: {
+    backgroundColor: "#ede9e9b5",
+    display: "flex",
+    alignItems: "center",
+    // justifyContent: "space-between",
+    paddingLeft: 20,
+    minHeight: "78px",
+    gap: theme.spacing(2),
+  },
+  rightbarBottom: {
+    overflow: "auto",
+    width: "100%",
+    flex: 1,
+    display: "flex",
+    flexDirection: "column",
+    height: "100%",
+  },
+  image: {
+    width: 300,
+    height: 300,
+    borderRadius: "50%",
+    objectFit: "cover",
+    border: "1px solid #ccc",
+  },
+  title: {
+    fontWeight: "normal",
+    fontSize: "30px",
+    display: "block",
+    textAlign: "center",
+    width: "100%",
+  },
+}));
