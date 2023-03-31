c6e535ca0fd2d26fe07f734377393ffdf7644c55
Ivan Tom <77198289+Tomdieu@users.noreply.github.com>
2023-03-31 23:52:11 +0200

    updated websocket url path

--- backend/account/routing.py
+++ backend/account/routing.py
@@ -2,5 +2,5 @@ from django.urls import re_path
 from .consumers import UserConsumer
 
 websocket_urlpatterns = [
-    re_path(r"ws/notification/(?P<user_id>[0-9]+)(./)?$", UserConsumer.as_asgi())
+    re_path(r"ws/notification/(?P<user_id>[0-9]+)/$", UserConsumer.as_asgi())
 ]
