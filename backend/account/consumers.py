4d444f2ce016f9f01c4a4e7855be2bd07455993f
Ivan Tom <77198289+Tomdieu@users.noreply.github.com>
2023-04-03 07:42:52 +0200

    remove unused imports

--- backend/account/consumers.py
+++ backend/account/consumers.py
@@ -5,10 +5,8 @@ import json
 
 from django.contrib.auth import get_user_model
 
-from .models import UserProfile
 
 from channels.db import database_sync_to_async
-from asgiref.sync import sync_to_async
 
 
 User = get_user_model()
@@ -21,12 +19,12 @@ class UserConsumer(AsyncWebsocketConsumer):
 
         user = self.scope["user"]
 
-        # Set the user online
-        await self.set_user_online(user)
-
         # Join room conversation
         await self.channel_layer.group_add(self.room_group_name, self.channel_name)
         await self.accept()
+
+        # Set the user online
+        await self.set_user_online(user)
 
     async def disconnect(self, code):
         user = self.scope["user"]
@@ -39,19 +37,23 @@ class UserConsumer(AsyncWebsocketConsumer):
     async def receive(self, text_data=None, bytes_data=None):
         text_data_json: dict = json.loads(text_data)
         message = text_data_json["message"]
-        type = text_data_json.get("msgType", None)
+        msgType = text_data_json.get("msgType", None)
+
+        print("Text Data Json : ", text_data_json)
 
         # Send message to room group
         await self.channel_layer.group_send(
             self.room_group_name,
-            {"type": "send_notification", "message": message, "msgType": type},
+            {"type": "send_notification", "message": message, "msgType": msgType},
         )
 
     async def send_notification(self, event: dict):
         message = event["message"]
-        type = event["msgType"]
+        msgType = event["msgType"]
 
-        self.send(text_data=json.dumps({"message": message, "msgType": type}))
+        print("Event : ", event)
+
+        await self.send(text_data=json.dumps({"message": message, "msgType": msgType}))
 
     @database_sync_to_async
     def set_user_online(self, user: User):
